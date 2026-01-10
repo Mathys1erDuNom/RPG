37ff32e39fece88d5f19d0e328ebdde4fa9327c7
MathStrike <92716055+MathStrike@users.noreply.github.com>
2026-01-10 19:25:55 +0100

    variable

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-VOICE_CHANNEL_ID = int(os.getenv("VOICE_CHANNEL_ID_COPAING"))
+VOICE_CHANNEL_ID = int(os.getenv("CHANNEL_ID_COPAING"))
 
 intents = discord.Intents.default()
 intents.message_content = True
