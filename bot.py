1c992b636f7a1dd9d0a98eb65757cf17ba8f63e4
MathStrike <92716055+MathStrike@users.noreply.github.com>
2026-01-10 20:04:06 +0100

    k

--- bot.py
+++ bot.py
@@ -6,15 +6,23 @@ from combat import CombatView
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID_COPAING"))  # <-- ID du salon où envoyer le message
 
 intents = discord.Intents.default()
-
+intents.message_content = True  # nécessaire pour lire et envoyer les messages
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"✅ Bot connecté en tant que {bot.user}")
+
+    # Récupérer le salon par son ID
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send("🟢 **Le bot est connecté et prêt !** 🐊")
+    else:
+        print("❌ Salon introuvable (ID incorrect ou bot n'a pas les permissions)")
 
 @bot.command()
 async def combat(ctx):
