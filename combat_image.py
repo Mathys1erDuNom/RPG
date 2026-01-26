3317718bd9f1f8a463c3ad22f35517983aaa4d91
MathStrike <92716055+MathStrike@users.noreply.github.com>
2026-01-26 14:46:14 +0100

    better image

--- combat_image.py
+++ combat_image.py
@@ -20,8 +20,8 @@ def creer_image_combat(joueur, ennemi, fond_path="images/region/fond.png"):
     ennemi_img = ennemi_img.resize((350, 350))
     
     # Coller sur le fond
-    fond.paste(perso_img, (200, fond.height - 300), perso_img)
-    fond.paste(ennemi_img, (fond.width - 400, fond.height - 300), ennemi_img)
+    fond.paste(perso_img, (200, fond.height - 350), perso_img)
+    fond.paste(ennemi_img, (fond.width - 400, fond.height - 350), ennemi_img)
     
     # Dessiner barres de PV
     draw = ImageDraw.Draw(fond)
